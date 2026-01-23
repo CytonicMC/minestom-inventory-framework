f99a3c5a28386d9b461a37f17b0d54d888a5ed45
Webhead1104 <webhead1104@hotmail.com>
2026-01-22 18:26:47 -0600

    Remove root build.gradle.kts

--- minestomInventoryFramework/build.gradle.kts
+++ minestomInventoryFramework/build.gradle.kts
@@ -5,7 +5,14 @@ plugins {
 }
 
 group = "net.cytonic"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
 
 repositories {
     mavenCentral()
@@ -14,4 +21,43 @@ repositories {
 dependencies {
     compileOnly(libs.minestom)
     api(libs.platform)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "FoxikleCytonicRepository"
+            url = uri("https://repo.foxikle.dev/cytonic")
+            // Use providers to get the properties or fallback to environment variables
+            var u = System.getenv("REPO_USERNAME")
+            var p = System.getenv("REPO_PASSWORD")
+
+            if (u == null || u.isEmpty()) {
+                u = "no-value-provided"
+            }
+            if (p == null || p.isEmpty()) {
+                p = "no-value-provided"
+            }
+
+            val user = providers.gradleProperty("FoxikleCytonicRepositoryUsername").orElse(u).get()
+            val pass = providers.gradleProperty("FoxikleCytonicRepositoryPassword").orElse(p).get()
+            credentials {
+                username = user
+                password = pass
+            }
+            authentication {
+                create<BasicAuthentication>("basic") {
+
+                }
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
 }
